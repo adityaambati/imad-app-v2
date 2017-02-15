43f2b37a0884750da324bd9f8751135730214021
adityaambati <adityarockrock123@gmail.com>
2017-02-15 20:12:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
+
+app.get('/article-one',function(req, res){
    res.send('Aritle one is requested and will be served here'); 
 });
-app.get('/article-two',function(req,res){
+app.get('/article-two',function(req, res){
    res.send('Aritle two is requested and will be served here'); 
 });
-app.get('/article-three',function(req,res){
+
+app.get('/article-three',function(req, res){
    res.send('Aritle three is requested and will be served here'); 
 });
 
